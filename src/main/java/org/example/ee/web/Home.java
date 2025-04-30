548e08a8eefe74a273662a46fb15d73df7521762
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2025-05-01 02:38:48 +0530

    web module

---
+++ src/main/java/org/example/ee/web/Home.java
@@ -0,0 +1,32 @@
+package org.example.ee.web;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.ee.ejb.remote.UserDetails;
+
+import javax.naming.InitialContext;
+import java.io.IOException;
+
+@WebServlet("/home")
+public class Home extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+        try {
+
+            InitialContext initialContext = new InitialContext();
+            UserDetails userDetails = (UserDetails) initialContext.lookup("java:global/ejb-module/UserDetailsBean");
+
+            resp.getWriter().println("Hello, "+userDetails.getName());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
