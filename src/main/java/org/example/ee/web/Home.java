5519e02e680901612f2e3c7a0a0e597236b7e074
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2025-05-01 13:00:29 +0530

    web module

--- src/main/java/org/example/ee/web/Home.java
+++ src/main/java/org/example/ee/web/Home.java
@@ -1,5 +1,6 @@
 package org.example.ee.web;
 
+import jakarta.ejb.EJB;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -13,20 +14,24 @@ import java.io.IOException;
 @WebServlet("/home")
 public class Home extends HttpServlet {
 
+    @EJB
+    private UserDetails userDetails;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-
-        try {
-
-            InitialContext initialContext = new InitialContext();
-            UserDetails userDetails = (UserDetails) initialContext.lookup("java:global/ejb-module/UserDetailsBean");
+//        try {
+//
+//            InitialContext initialContext = new InitialContext();
+//            UserDetails userDetails = (UserDetails) initialContext.lookup("java:global/ejb-module/UserDetailsBean");
+//
+//            resp.getWriter().println("Hello, "+userDetails.getName());
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
 
             resp.getWriter().println("Hello, "+userDetails.getName());
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 
     }
 }
