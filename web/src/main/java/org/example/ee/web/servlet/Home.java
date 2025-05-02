e42b025c5a9dd056365b1633fd7f1ab4938d99ea
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2025-05-03 01:44:36 +0530

    ear root

--- web/src/main/java/org/example/ee/web/servlet/Home.java
+++ web/src/main/java/org/example/ee/web/servlet/Home.java
@@ -1,19 +1,32 @@
 package org.example.ee.web.servlet;
 
+import jakarta.ejb.EJB;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.example.ee.ejb.remote.ProductService;
 
 import java.io.IOException;
 
 @WebServlet("/home")
 public class Home extends HttpServlet {
 
+    @EJB
+    private ProductService productService;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        resp.setContentType("text/html");
         resp.getWriter().println("<h1>Home</h1>");
+        productService.getAllProducts().forEach(p->{
+            try {
+                resp.getWriter().write(p.getName());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
