311f633f585ca5913e661ae6c659aad65ea07f7b
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2025-05-03 19:14:46 +0530

    jms first client

---
+++ src/main/java/org/example/ee/jms/App.java
@@ -0,0 +1,47 @@
+package org.example.ee.jms;
+
+import jakarta.jms.*;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        try {
+            InitialContext context = new InitialContext();
+            TopicConnectionFactory factory = (TopicConnectionFactory) context.lookup("jms/MyConnectionFactory");
+
+            TopicConnection connection = factory.createTopicConnection();
+            connection.start();
+
+            Topic topic = (Topic) context.lookup("jms/MyTopic");
+
+            TopicSession session = connection.createTopicSession(false, Session.AUTO_ACKNOWLEDGE);
+            TopicPublisher publisher = session.createPublisher(topic);
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter your message or type 'exit' to exit:");
+
+            while (true){
+
+                String line = scanner.nextLine();
+                if(line.equalsIgnoreCase("exit")){
+                    break;
+                }
+
+                TextMessage message = session.createTextMessage();
+                message.setText(line);
+
+                publisher.publish(message);
+            }
+            scanner.close();
+
+        } catch (NamingException | JMSException e) {
+            e.printStackTrace();
+        }
+    }
+}
