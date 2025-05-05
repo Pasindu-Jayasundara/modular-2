b44b7c2740e7fa1da7da09e66e9d76d1c120b44e
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2025-05-05 17:19:49 +0530

    jms second client

--- src/main/java/org/example/ee/client/QueueReceiver.java
+++ src/main/java/org/example/ee/client/QueueReceiver.java
@@ -5,41 +5,40 @@ import jakarta.jms.*;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
-public class App {
+public class QueueReceiver {
 
     public static void main(String[] args) {
 
         try {
-            InitialContext context = new InitialContext();
-            TopicConnectionFactory factory = (TopicConnectionFactory) context.lookup("jms/MyConnectionFactory");
 
-            TopicConnection connection = factory.createTopicConnection();
+            InitialContext context = new InitialContext();
+            QueueConnectionFactory factory = (QueueConnectionFactory) context.lookup("jms/MyQueueConnectionFactory");
+
+            QueueConnection connection = factory.createQueueConnection();
             connection.start();
 
-            Topic topic = (Topic) context.lookup("jms/MyTopic");
+            Queue queue = (Queue) context.lookup("jms/MyQueue");
 
-            TopicSession session = connection.createTopicSession(false, Session.AUTO_ACKNOWLEDGE);
-            TopicSubscriber subscriber = session.createSubscriber(topic);
+            QueueSession session = connection.createQueueSession(false, Session.CLIENT_ACKNOWLEDGE);
+            jakarta.jms.QueueReceiver receiver = session.createReceiver(queue);
 
-            subscriber.setMessageListener(new MessageListener() {
+//            Message receive = receiver.receive();
+            receiver.setMessageListener(new MessageListener() {
                 @Override
                 public void onMessage(Message message) {
-
                     try {
                         System.out.println(message.getBody(String.class));
+                        message.acknowledge();
                     } catch (JMSException e) {
                         e.printStackTrace();
                     }
-
                 }
             });
 
-            while (true){
-            }
+            while (true) {}
 
         } catch (NamingException | JMSException e) {
             e.printStackTrace();
         }
     }
-
 }
